69ef154af6b8b5fc03fa3c0d87227ee2d8683244
naamaCohen1 <naamacohen26@gmail.com>
2020-06-17 22:47:34 +0300

    changing place

---
+++ Memory_Game_files/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+namespace B20_Ex02_01
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            runApp();
+        }
+
+        private static void runApp()
+        {
+            CharValues values = new CharValues();
+            UserActivety<char?> userActivity = new UserActivety<char?>();
+            Player firstPlayer = new Player();
+            Player secondPlayer = new Player();
+            int length = 0, hight = 0;
+            bool newGame = true;
+
+            userActivity.GetMainPlayerInput(ref firstPlayer, ref secondPlayer);
+            while(newGame)
+            {
+                userActivity.GetBoardSize(ref length, ref hight);
+                Board<char?> board = new Board<char?>(length, hight, values.CharList);
+                Game<char?> game = new Game<char?>(board, ref firstPlayer, ref secondPlayer);
+                userActivity.PrintBoard(board);
+                game.PlayGame();
+                newGame = userActivity.StartNewGame();
+            }
+
+            userActivity.Print("****** Game Over! Bye Bye ******");
+        }
+    }
+}
